1006fc40352af4d2b6dd6cab21430f98d1c3d06a
Vaibhav <vaibhavsingh2GMAIL.COM>
2024-07-19 00:48:55 +0530

    Fixed CSRF issue and made a basic navbar

--- mywebapp/authen/views.py
+++ mywebapp/authen/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import *
@@ -57,3 +57,6 @@ def register_page(request):
     
     return render(request, 'register.html')
 
+def logout_view(request):
+    logout(request)
+    return redirect('/login/')
--- mywebapp/mywebapp/urls.py
+++ mywebapp/mywebapp/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('home/', home, name="home_page"),      
     path("admin/", admin.site.urls),          
     path('login/', login_page, name='login_page'),    
-    path('register/', register_page, name='register_page'),  
+    path('register/', register_page, name='register_page'), 
+    path('logout/', logout_view, name='logout'), 
 ]    
  
 urlpatterns += staticfiles_urlpatterns()
